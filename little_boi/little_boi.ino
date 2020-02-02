eb2de32a3a9a8c0da4023a95c29c54aac60ce5ca
Daniel Castle <dcastle4@students.kennesaw.edu>
2020-02-02 10:50:10 -0500

    Created "finished" version of little_boi.ino. Still needs proper testing

--- little_boi/little_boi.ino
+++ little_boi/little_boi.ino
@@ -1,31 +1,129 @@
+/*
+ * 	File:little_boi.ino
+ * 	Authors: Daniel Castle & Spencer Mason
+ * 	Description: 
+ * 		Implementation of button pusing robot for SouthEastCon 2020
+ */
 
 
 // pi10000.h: header file containing 100000 digits of Pi
 // taken from official SouthEastCon2020 repo
 #include "pi10000.h"
 
+const enum START = 0;
+const enum PUSH_TO_WALL = 1;
+const enum EXECUTE = 2;
+
+unsigned long time;
+int index = 0;
+int currentState = START;
+bool masterSwitch;
+
+
 void setup() {
-  // put your setup code here, to run once:
-
-  Serial.begin(9600);
-
-  int maxIndex = 4;
-  
-  Serial.println("Print digits of Pi from index 0 to maxIndex-1:\n");
-  for (int i = 0; i < maxIndex; i++)  {
-    Serial.print(getPiDigit(i));
-  }
-
-  
-
+	
+	initPins(); // initialize pins 1 through 10
+	
+	// initialize pin 11 as input for master switch
+	pinMode(11, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+	// read input from pin 11 each loop to wait for master switch turning on
+	masterSwitch = digitalRead(11);
+	
+	// keep track of time running using millis() function
+	time = millis();
+	
+	
+	if(time < 180000) {
+		
+		switch(currentState) {
+			
+			
+			// START: wait for masterSwitch to turn on then goto PUSH_TO_WALL
+			case START:
+				{if( masterSwitch = true) {currentState = PUSH_TO_WALL;} }
+			break;
+			
+			
+			// PUSH_TO_WALL: launch solenoid on pin 10, which will tip the bot
+			case PUSH_TO_WALL:
+				{
+					pushSolenoid(10);
+					delay(1000);
+					pullSolenoid(10);
+					currentState = LOOP;
+				}
+			break;
+			
+			
+			// EXECUTE: execute push button logic
+			case EXECUTE:
+				{
+					pushSolenoid(getPiDigit(index));
+					holdSolenoid();
+					pullSolenoid(getPiDigit(index));
+					index++;
+				}
+			break;
+			
+			
+		}
+	}
+	
 }
 
 
+/*
+ * 	pushSolenoid:
+ * 		Sends a LOW signal to a power MOSFET which allows current to
+ * 		flow to solenoid which powers the 'pushing' mechanism.
+ */
+void pushSolenoid(int pin) {
+	digitalWrite(pin, LOW);
+}
+
+
+/*
+ * 	pullSolenoid:
+ * 		Sends a HIGH signal to a power MOSFET which prevents current
+ * 		from flowing to solenoid which retracts the 'pushing' mechanism.
+ */
+void pullSolenoid(int pin) {
+	digitalWrite(pin, HIGH);
+}
+
+
+/*
+ * 	holdSolenoid:
+ * 		Causes a 30ms delay to allow for the recieveing buttons to 
+ * 		debounce properly.
+ */
+void holdSolenoid() {
+	delay(30000);
+}
+
+
+/*
+ * 	initPins:
+ * 		Initializes all digital pins on UNO using
+ * 		a simple for loop. Can be changed later depending
+ * 		on pin usage or transition to MEGA board.
+ * 		(Pin 13 is not enabled due to issues with LED connetion.)
+ * 		(See 'NOTE' here: https://www.arduino.cc/en/Tutorial/DigitalPins)
+ * 		[Pin 11 will be used as input for master switch, so it is not 
+ * 		initialized here.]
+ */
+void initPins() {
+  // iterate through 1 to 12
+  for (int i = 0; i <= 10; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+}
+
+  
 /*
  *  getPiDigit:
  *    Takes in an index as a integer and returns the
@@ -34,16 +132,12 @@ void loop() {
  *    This function will fetch the Pi digit from
  *    the pi100000.h file provided by SouthEastCon.
  *    
- */
- 
+ */ 
 int getPiDigit(int index) {
-  
-  /* read byte at the address of pi + index 
-  *(address of pi means starting address of pi char array)
-  * the "- '0' " part casts this to an integer
-  * taken from SouthEastCon implementation 
-  */
-  
-  return pgm_read_byte_near(pi + index) - '0';
-  
+	/* read byte at the address of pi + index 
+	*(address of pi means starting address of pi char array)
+	* the "- '0' " part casts this to an integer
+	* taken from SouthEastCon implementation 
+	*/
+	return pgm_read_byte_near(pi + index) - '0';
 }
