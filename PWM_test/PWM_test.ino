6fd93406065c6f1860d154e1fdf8dfc363b4a979
Trevor <tstanca@students.kennesaw.edu>
2020-02-18 21:02:59 -0500

    added test file

---
+++ PWM_test/PWM_test.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(6, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(6, 0);
+  // put your main code here, to run repeatedly:
+ /* for(int i = 255; i <= 255; i--){
+  analogWrite(6, 255);
+  }
+  */
+}
