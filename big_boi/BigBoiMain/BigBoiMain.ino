19b3ff88712fb8818a7721c5e9427357a0752e87
Trevor <tstanca@students.kennesaw.edu>
2020-03-10 18:10:39 -0400

    added to big boi

--- big_boi/BigBoiMain/BigBoiMain.ino
+++ big_boi/BigBoiMain/BigBoiMain.ino
@@ -1,28 +1,56 @@
 // includes
 /**********************************************************************/
 
+
 // struct definitions
 /**********************************************************************/
 
 // global variable definitions
 /**********************************************************************/
-enum stateMachine {moveToBin, pickUp, moveToEnd, plop} state;
+enum stateMachine {next, moveToBin, pickUp, moveToEnd, plop} state;
+
+struct location {
+		int bottomLeftX;
+		int bottomLeftY;
+		int legosTaken;
+		int binIndex;
+		bool onTop;
+	};
+location one, two, three, four, five, six, seven, eight, nine, zero, end;
+
+int pistonR, pistonL;
 
 float currX, currY;		// best effor estimations
-float diffX, diffY;		// set by diff function
+float percentX, percentY;		// set by diff function
 // Piston Functions
 /**********************************************************************/
+void pushSolenoid(int pin) {
+	digitalWrite(pin, LOW);
+}
 
+void pullSolenoid(int pin) {
+	digitalWrite(pin, HIGH);
+}
+
+void grab() {
+	pushSolenoid(pistonR);
+	pushSolenoid(pistonL);
+}
+
+void yeet() {
+	pushSolenoid(pistonR);
+	pushSolenoid(pistonL);
+}
 // Low Level Motor Functions
 /**********************************************************************/
 
 // Higher Level Wheel Movement Functions -- including throttle
 /**********************************************************************/
-
+percentDiff(location target);
 // Grabber Functions
 /**********************************************************************/
 void setup() {
-	state = moveToBin;
+	state = next;
 
 }
 
@@ -33,7 +61,7 @@ void loop() {
 	// moveToEnd moves in the Y direction then X direction
 	switch(state) {
 		case moveToBin:
-			// perform Bin Movement
+			percentDiff()
 			state = pickUp;
 			break;
 		case pickUp:
