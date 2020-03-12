9018c52dd4871d46e3160bac9af6e77052c87e44
Noah <noahtrinite@gmail.com>
2020-03-11 21:46:24 -0400

    changed the pins being used since 1 and 0 can't be used so now it starts at pin 2 - 11. Pin 12 requires a high signal to start the sequence

--- tests/Test2.ino
+++ tests/Test2.ino
@@ -2,13 +2,13 @@
 #include <Servo.h>
 
 int index = 0;
-int masterSwitch;
+bool onSwitch;
 
 Servo myServo;
 
 void initPins() {
   // iterate through 0 to 10
-  for (int i = 1; i <= 10; i++)
+  for (int i = 0; i <= 12; i++)
   {
     pinMode(i, OUTPUT);
   }
@@ -24,11 +24,19 @@ int getPiDigit(int index) {
 }
 
 void PowerGOGO() {
-  digitalWrite(getPiDigit(index), HIGH);
+  digitalWrite(getPiDigit(index)+2, HIGH);
   delay(30);
-  digitalWrite(getPiDigit(index), LOW);
+  digitalWrite(getPiDigit(index)+2, LOW);
   delay(30);
+
   index++;
+
+ /* for(int i =2; i<12; i++){
+    digitalWrite(i, HIGH);
+    delay(3000);
+    digitalWrite(i, LOW);
+    delay(3000);
+  }*/
 
 }
 
@@ -43,31 +51,35 @@ void ZaWarudo() {
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
   digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  //digitalWrite(12, LOW);
 
 }
 
 void setup() {
   // put your setup code here, to run once:
   initPins(); // initialize pins 1 through 10
-  myServo.attach(12);
+  //myServo.attach(12);
 
   // initialize pin 11 as input for master switch
-  pinMode(11, INPUT);
-  startTime = millis();
+  //pinMode(13, INPUT);
+  //startTime = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(11) == LOW) {
-     PowerGOGO();
-     Serial.println("This line is high");
+  if (digitalRead(12) == HIGH) {
+    onSwitch = true;
     //ZaWarudo();
-   
-
   }
   else {
     //PowerGOGO();
     Serial.println("This line is low");
+    onSwitch =false;
     ZaWarudo();
+  }
+
+  while(onSwitch){
+    PowerGOGO();
   }
 }
